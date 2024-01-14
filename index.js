4394be2c12203a7c9e4b3fce2988fdf35706e323
vishalsoni7 <117621655+vishalsoni7@users.noreply.github.com>
2024-01-14 15:18:16 +0000

    refactor

--- index.js
+++ index.js
@@ -10,20 +10,11 @@ const pruductRouter = require("./router/product.routes");
 
 initializeDatabase();
 
-app.use(cors());
-
-// app.use(
-//   cors({
-//     origin: "*",
-//   }),
-// );
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("e-commerce");
