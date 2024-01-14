584f0bc75f1c57f2e74d54da42f93b1a6480531f
vishalsoni7 <117621655+vishalsoni7@users.noreply.github.com>
2024-01-14 15:06:16 +0000

    refactor

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.use(cors());
 //   }),
 // );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("e-commerce");
 });
